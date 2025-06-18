fe48326ba8f7936aa325a5a2624880ef2750c22e
Bishan Bhandari <108720684+bishanbhandari@users.noreply.github.com>
2025-06-18 14:23:19 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+const App = () => {
+  return (
+    <div className="overflow-x-hidden text-neutral-300 ">
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <Navbar />
+    </div>
+  );
+};
 
-function App() {
-  
-}
-
-export default App
+export default App;
