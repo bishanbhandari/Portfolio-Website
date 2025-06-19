7d895a33ea1787acdd45ba8f1aa296392647b2ad
Bishan Bhandari <108720684+bishanbhandari@users.noreply.github.com>
2025-06-19 15:08:28 +0545

    changes

---
+++ src/Components/HeroSection.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section
+      id="hero"
+      className="relative min-h-screen flex flex-col items-center justify-center px-4"
+    >
+      <div className="container max-w-4xl mx-auto text-center z-10">
+        <div className="space-y-6">
+          <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+            <span className="opacity-0 animate-fade-in">Hi, I'm </span>
+            <span className="text-primary opacity-0 animate-fade-in-delay-1">
+             Bishan{" "}
+            </span>
+            <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">Bhandari
+            </span>
+          </h1>
+
+          <p className="text-lg md:text-xl  text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+            I'm a frontend developer with skills in HTML, CSS, JavaScript and
+            React. I build clean, responsive, and interactive web interfaces,
+            always aiming to learn and take on new challenges.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,18 +4,20 @@ import { FaLinkedin } from "react-icons/fa";
 import { VscGithub } from "react-icons/vsc";
 import { FaInstagram } from "react-icons/fa";
 import { cn } from "../lib/util";
-
+import { TbXboxX } from "react-icons/tb";
+import { IoMdMenu } from "react-icons/io";
 
 const Navbar = () => {
   const navItems = [
     { name: "Home", href: "#hero" },
     { name: "About", href: "#about" },
-    { name: "Skills", href: "skills" },
+    { name: "Skills", href: "#skills" },
     { name: "Projects", href: "#projects" },
     { name: "Contact", href: "#contact" },
   ];
 
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -33,23 +35,55 @@ const Navbar = () => {
       )}
     >
       <div className="container flex items-center justify-between">
-      <a href="#hero">
-         <h1
-          className="flex justify-center items-center font-bold text-2xl"
-        >
-          BishanPortfolio
-          <FiMonitor />
-        </h1>
+        <a href="#hero">
+          <h1 className="flex justify-center items-center font-bold text-2xl">
+            BishanPortfolio
+            <FiMonitor />
+          </h1>
         </a>
-        {/*pc*/}
+        {/*pc nav*/}
         <div className=" hidden md:flex space-x-8">
-          {navItems.map((item,key) => (
-              <a key={key} href={item.href} className="hover:text-primary transition-colors duration-300">
-                {item.name}
-              </a>
+          {navItems.map((item, key) => (
+            <a
+              key={key}
+              href={item.href}
+              className="hover:text-primary transition-colors duration-300"
+            >
+              {item.name}
+            </a>
           ))}
         </div>
 
+        {/* mobile nav */}
+
+        <button onClick={() => setIsMenuOpen((prev) => !prev)}
+          className="md:hidden p-2 z-50 cursor-pointer"
+          >
+          {isMenuOpen ? <TbXboxX size={24} /> : <IoMdMenu size={24} />}
+        </button>
+
+        <div
+          className={cn(
+            "fixed inset-0 backdrop-blur-md z-40 flex flex-col items-center justify-center",
+            "transition-all duration-300 md:hidden",
+            isMenuOpen
+              ? "opactity-100 pointer-events-auto"
+              : "opacity-0 pointer-events-none"
+          )}
+        >
+          <div className="flex flex-col space-y-8">
+            {navItems.map((item, key) => (
+              <a
+                key={key}
+                href={item.href}
+                className="hover:text-primary transition-colors duration-300"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </div>
       </div>
     </nav>
   );
