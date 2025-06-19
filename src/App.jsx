a9b021b64fd617d519d370d0ccce3f7c4d53b09e
Bishan Bhandari <108720684+bishanbhandari@users.noreply.github.com>
2025-06-19 11:56:57 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,8 @@ import ThemeToggle from "./Components/ThemeToggle";
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 ">
-    <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+    { <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div> }
       <Navbar/>
-      <ThemeToggle/>
     </div>
   );
 };
--- src/Components/ThemeToggle.jsx
+++ src/Components/ThemeToggle.jsx
@@ -1,25 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { CiSun } from "react-icons/ci";
 import { FaRegMoon } from "react-icons/fa";
+import { cn } from "../lib/util";
 
 const ThemeToggle = () => {
+  const [isDarkMode, setisDarkMode] = useState(false);
 
-    const [isDarkMode,setisDarkMode]=useState(false);
-
-    const toggleTheme=()=>{
-        if(isDarkMode){
-            setisDarkMode(false);
-        }
-        else{
-            document.documentElement.classList.add("dark");
-            setisDarkMode(true);
-        }
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme === "dark") {
+      setisDarkMode(true);
+      document.documentElement.classList.add("dark");
+    } else {
+      localStorage.setItem("theme", "light");
+      setisDarkMode(false);
     }
+  }, []);
 
+  const toggleTheme = () => {
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+      setisDarkMode(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      setisDarkMode(true);
+    }
+  };
 
   return (
-    <button onClick={toggleTheme}>{isDarkMode?<CiSun className='h-6 w-6 text-yellow-300'/>:<FaRegMoon className='h-6 w-6 text-blue-300'/>}</button>
-  )
-}
+    <button
+      onClick={toggleTheme}
+      className={cn(
+        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outline-hidden"
+      )}>
+      {isDarkMode ? (
+        <CiSun className="h-6 w-6 text-yellow-300" />
+      ) : (
+        <FaRegMoon className="h-6 w-6 text-blue-300" />
+      )}
+    </button>
+  );
+};
 
 export default ThemeToggle;
